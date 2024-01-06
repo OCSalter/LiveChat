9986da6870ea4ee8475a500e2aca00318780314f
owen <owensalter@outlook.com>
2024-01-06 17:17:29 -0500

    Add server header contents

--- Server.h
+++ Server.h
@@ -11,15 +11,22 @@ class Server
 {
     public:
         void run(uint16 nPort);
+        void localUserInput(std::string cmd);
         
     private:
+        bool active;
         HSteamListenSocket listenSocket;
 	    HSteamNetPollGroup pollGroup;
 	    ISteamNetworkingSockets *sockets;
 
         std::map<HSteamNetConnection, std::string> clientMap;
 
+        void sendStringToClient(HSteamNetConnection conn, const char *str);
+        void sendStringToAll(const char *std, HSteamNetConnection except = k_HSteamNetConnection_Invalid);
+        void pollIncomingMessages();
+        void setClientName(HSteamNetConnection conn, const char *name);
         void onConnectionStatusChanged(SteamNetConnectionStatusChangedCallback_t *info);
+        void pollConnectionStateChanges();
 
         static Server *callbackInstance;
         static void connectionStatusChangedCallback(SteamNetConnectionStatusChangedCallback_t *info)
